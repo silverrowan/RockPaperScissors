59a2a70a05c4412617a23b9c40a550feb0e19200
SilverRowan <silverrowan@gmail.com>
2025-03-31 21:23:40 -0700

    Create JS for user prompt, add variable to pseudocodeplanning

--- javascript.js
+++ javascript.js
@@ -10,3 +10,21 @@ function getComputerChoice () { // Create the function named "getComputerChoice"
 }
 getComputerChoice();               // CALL getComputerChoice function
 console.log(getComputerChoice);    // Print value of getComputerChoice to the console
+
+
+function getHumanChoice () {    //     Create function getHumanChoice
+    let humanChoice = prompt( "Choose & enter one of: Rock Paper or Scissors");     // Create variable humanChoice, Prompt User to "Enter Rock Paper or Scissors"
+    let trimHumanChoice = humanChoice.trim().toLowerCase();     // Create variable trimHumanChoice & Transform answer to lowercase and trim off whitespaces
+    console.log(trimHumanChoice);
+}
+getHumanChoice();
+console.log(getHumanChoice)
+
+
+    //  - assume will correctly enter choice
+        // 
+        // IF any of rock paper scissors then assign to humanChoice 
+            // (Print value of getHumanChoice to console)
+        // ELSE Prompt Incorrect Entry, please try again
+        // ENDIF
+    // Call getHumanChoice function
