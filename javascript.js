bfc18f8a7ef850282fa6ae27ed1b139403ea4093
SilverRowan <silverrowan@gmail.com>
2025-03-31 21:57:24 -0700

    Write Code for chooseWinner function & call it

--- javascript.js
+++ javascript.js
@@ -11,20 +11,21 @@ function getComputerChoice () { // Create the function named "getComputerChoice"
 getComputerChoice();               // CALL getComputerChoice function
 console.log("Computer: " + getComputerChoice);    // Print value of getComputerChoice to the console
 
-
 function getHumanChoice () {    //     Create function getHumanChoice
     let humanChoice = prompt( "Choose & enter one of: Rock Paper or Scissors");     // Create variable humanChoice, Prompt User to "Enter Rock Paper or Scissors"
     getHumanChoice = humanChoice.trim().toLowerCase();     // Create variable trimHumanChoice & Transform answer to lowercase and trim off whitespaces
     //normally would check that its actually one of the options, but not bothering here, as instructed.
 }
-getHumanChoice();
-console.log("User: " + getHumanChoice)
+getHumanChoice();       // Call getHumanChoice function
+console.log("User: " + getHumanChoice)  //Print user choice to console
 
-
-    //  - assume will correctly enter choice
-        // 
-        // IF any of rock paper scissors then assign to humanChoice 
-            // (Print value of getHumanChoice to console)
-        // ELSE Prompt Incorrect Entry, please try again
-        // ENDIF
-    // Call getHumanChoice function
+function chooseWinner () {  // Create chooseWinner function - to compare human and computer choices
+    if (getComputerChoice === getHumanChoice) {     // IF the getComputerChoice and getHumanChoice have the same value
+        console.log("Tie")    // THEN print "Tie" to the console. 
+    } else if (getComputerChoice === "rock" && getHumanChoice === "paper" ||         // IF getComputerChoice is rock and getHumanChoice is paper, 
+        getComputerChoice === "paper" && getHumanChoice === "scissors" ||            // or getComputerChoice is paper and getHumanChoice is scissors,
+        getComputerChoice === "scissors" && getHumanChoice === "rock") {          // or compgetComputerChoice is scissors and getHumanChoice is rock
+            console.log("User wins!")        // THEN print "User Wins"
+    } else { console.log("Computer Wins!")}        // ELSE print "Computer Wins"
+}
+chooseWinner();
