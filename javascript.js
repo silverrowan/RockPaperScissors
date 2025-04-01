655e1b1dbb19651685bc9e4e18084992be706d07
SilverRowan <silverrowan@gmail.com>
2025-03-31 20:52:40 -0700

    Complete computer choice function.
    now prints the final random "choice" as one of
    "Rock" "Paper" or "Scissors"

--- javascript.js
+++ javascript.js
@@ -1,20 +1,13 @@
 function getComputerChoice () { // Create the function named "getComputerChoice"
     let choiceNum = Math.floor(Math.random() * 3);     // Create choiceNum variable & Assign a random number from 0-2 
     console.log(choiceNum);
+    if (choiceNum === 0) {        // IF  choiceNum = 0
+        getComputerChoice = "Rock";             // Assign getComputerChoice "Rock"
+    } else if (choiceNum === 1) {         // ELSE IF choiceNum = 1
+        getComputerChoice = "Paper";            // Assign getComputerChoice "Paper"
+    } else {                                    // ELSE
+        getComputerChoice = "Scissors";         // Assign getComputerChoice "Scissors"
+    }                                           // ENDIF
 }
-getComputerChoice();
-
-
-
-
-
-
-    // IF  choiceNum = 0
-    // THEN
-        // Assign getComputerChoice "Rock"
-    // ELSE IF choiceNum = 1
-        // Assign getComputerChoice "Paper"
-    // ELSE
-        // Assign getComputerChoice "Scissors"
-    // ENDIF
-    // Print value of getComputerChoice to the console
+getComputerChoice();               // CALL getComputerChoice function
+console.log(getComputerChoice);    // Print value of getComputerChoice to the console
