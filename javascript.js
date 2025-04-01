fb75eaa9525396068924a284af9b24df00a15ced
SilverRowan <silverrowan@gmail.com>
2025-03-31 20:45:16 -0700

    Create random number sub-function (0-3)
     for computer choice of RPS

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function getComputerChoice () { // Create the function named "getComputerChoice"
+    let choiceNum = Math.floor(Math.random() * 3);     // Create choiceNum variable & Assign a random number from 0-2 
+    console.log(choiceNum);
+}
+getComputerChoice();
+
+
+
+
+
+
+    // IF  choiceNum = 0
+    // THEN
+        // Assign getComputerChoice "Rock"
+    // ELSE IF choiceNum = 1
+        // Assign getComputerChoice "Paper"
+    // ELSE
+        // Assign getComputerChoice "Scissors"
+    // ENDIF
+    // Print value of getComputerChoice to the console
