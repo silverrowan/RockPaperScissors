36ba164f6965939a7161cafcfa7dd5daa846a64d
SilverRowan <silverrowan@gmail.com>
2025-04-01 20:11:33 -0700

    Split getComputerChoice into two functions

--- javascript.js
+++ javascript.js
@@ -1,20 +1,28 @@
-function getComputerChoice () { 
-    let computerChoice;
-    let choiceNum = Math.floor(Math.random() * 3);     // Create choiceNum variable & Assign a random number from 0-2 
-    if (choiceNum === 0) {                             // IF  choiceNum = 0->"Rock", 1->"Paper", 2->"Scissors"
-        computerChoice = "rock";             
-    } else if (choiceNum === 1) {               
-        computerChoice = "paper";            
-    } else {                                    
-        computerChoice = "scissors";         
-    }                                          
+function getRandomNum (minValue = 0, maxValue = 100) {
+    let randomNum = Math.floor(Math.random() * ((maxValue-minValue) + 1))+ minValue; 
+    return randomNum;
+}
+
+function getComputerChoice () {
+    let computerChoice = getRandomNum(0,2);
+    switch (computerChoice) {
+        case 0: 
+            computerChoice = "rock";
+        break;
+        case 1: 
+            computerChoice = "paper";
+        break;
+        case 2: 
+            computerChoice = "scissors";
+        break;
+    }
     return computerChoice;
 }
 
 
 function getHumanChoice () {                      
     let humanChoiceRaw = prompt( "Choose & enter one of: Rock Paper or Scissors");
-    humanChoice = humanChoiceRaw.trim().toLowerCase();     // Create variable trimHumanChoice & transform answer to lowercase and trim off whitespaces
+    humanChoice = humanChoiceRaw.trim().toLowerCase(); // transform answer to lowercase and trim off whitespaces
     //normally would check that its actually one of the options & re-prompt if not, but not bothering here, as instructed.
     return humanChoice;
 }
@@ -24,10 +32,10 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playGame () {
-    function playRound (humanChoice, computerChoice) {                          // compare human and computer choices
+    function playRound (humanChoice, computerChoice) {  // compare human and computer choices; nested in playGame per instructions
         console.log("Computer: " + computerChoice);
         console.log("User: " + humanChoice);
-        if (computerChoice === humanChoice) {     // if same value, then report "Tie"
+        if (computerChoice === humanChoice) {
             console.log("Tie");
         } else if (computerChoice === "rock" && humanChoice === "paper" ||         // IF getComputerChoice is rock and getHumanChoice is paper, 
             computerChoice === "paper" && humanChoice === "scissors" ||            // or getComputerChoice is paper and getHumanChoice is scissors,
@@ -35,7 +43,7 @@ function playGame () {
                 console.log(`User wins, ${humanChoice} beats ${computerChoice}!`);               // THEN print "User Wins"
                 ++ humanScore;
         } else { 
-            console.log(`Computer Wins, ${computerChoice} beats ${humanChoice}!`);         // ELSE print "Computer Wins"
+            console.log(`Computer Wins, ${computerChoice} beats ${humanChoice}!`);
             ++ computerScore;
         }
         console.log("Score: User: " + humanScore + ", Computer: " + computerScore)
