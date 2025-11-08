e10eadcc6dae08f9932faeb0da2f0a2b444276a3
SilverRowan <silverrowan@gmail.com>
2025-11-08 02:39:29 -0800

    Improve comments, Refactor code for readability
    renamed assorted variables & functions for clarity
    Moved game-specific code out of global scope and into playGame()
    Split valid entry check, checkValidUserChoice(), out of roundWinner()
    Split getCleanResponse() out of getUserChoice()

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,64 +1,80 @@
-//Create function getComputerChoice
-function getComputerChoice() {
-    //declare function: get integer between values a and b, inclusive, a = lowest value, b=highest value
-    function randomInteger(a, b) { 
+//Choose a random number between (inclusive) "a" and "b"; 
+//where "a" is the lowest value, and "b" the highest
+function randomInteger(a, b) { 
         return Math.floor( Math.random() * b + a );
     }
-
-    //get randomInteger from 1-3, assign to computerChoice
-    let random123 = randomInteger( 1, 3 ); 
-    let randomRockPaperScissors;
-
-    //use computerChoice number to set Rock/paper/scissors
-    if (random123 === 1) { 
-        randomRockPaperScissors = "rock";
-    } else if (random123 === 2) {
-        randomRockPaperScissors = "paper";
-    } else if (random123 === 3) {
-        randomRockPaperScissors = "scissors";
-    } else {console.log("variable randomNumber out of bounds");}
-    return randomRockPaperScissors;
+//Clean string by: converting to lowercase, removing puctuation, and trimming any 
+//start or end whitespaces.
+function cleanText (text) {
+    return text.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").trim().toLowerCase();
 }
 
-//Create Function getHumanChoice
-function getHumanChoice() {
-    //Create Function cleanText, to trim, lowercase, and de-punctuate responce
-    function cleanText (text) {
-        return text.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").trim().toLowerCase();
-    }
-
-    //prompt user for choice and record in (new) variable userChoice, and clean the string
-    let userPromptChoice = cleanText( prompt("Choose one of: rock, paper, or scissors.", "") );
-    return userPromptChoice;
+//get responce from user and clean reply.
+function getCleanResponse( promptMessage, defaultResponse ) { 
+    let userResponse = cleanText( prompt(promptMessage, defaultResponse) );
+    return userResponse;
 }
 
-// CREATE win tracking variables
-let userWins = 0, computerWins = 0, ties = 0;
-
-// CREATE Function gameWinner: win logic & increase & user entry check
-function gameWinner(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-        console.log("You Tie.");
-        ties += 1;
-    } else if ( !( userChoice === "rock" || userChoice === "paper" || userChoice === "scissors" ) ) {
-        console.warn(`invalid entry, please choose one of rock, paper, or scissors next time.`); //checks user responce is a valid choice
-    } else if ( userChoice === "rock" && computerChoice === "scissors" 
-        || userChoice === "paper" && computerChoice === "rock" 
-        || userChoice === "scissors" && computerChoice === "paper" ) {
-            userWins += 1;
-            console.log(`You choose ${userChoice}, computer choose ${computerChoice}, You win!`);       
-    } else {
-        computerWins += 1;
-        console.log(`You choose ${userChoice}, computer choose ${computerChoice}, You loose.`);
-    } 
-}
 //CREATE function playGame: run 5 rounds of rock paper scissors and report the total scores
 function playGame() {
+    // CREATE win tracking variables
+    let userWins = 0, computerWins = 0, ties = 0;
+
     //CREATE function playRound
     function playRound () {
     let computerChoice = getComputerChoice();
-    let userChoice = getHumanChoice();
-    gameWinner(userChoice, computerChoice);
+    let userChoice = getUserChoice();
+    checkValidUserChoice(userChoice, computerChoice)
+    }
+
+    //have computer 'choose' random rock/paper/scissors via Math.random() method
+    function getComputerChoice() { 
+        let random123 = randomInteger( 1, 3 ); 
+        let randomRockPaperScissors;
+
+        //use random number to set Rock/paper/scissors
+        if (random123 === 1) { 
+            randomRockPaperScissors = "rock";
+        } else if (random123 === 2) {
+            randomRockPaperScissors = "paper";
+        } else if (random123 === 3) {
+            randomRockPaperScissors = "scissors";
+        } else {console.log("variable random123 out of bounds");}
+        return randomRockPaperScissors;
+    }
+
+    function getUserChoice() { //Ask user to type rock/paper/scissors & clean thier response
+        let userPromptChoice = getCleanResponse("Choose one of: rock, paper, or scissors.", "");
+        return userPromptChoice;
+    }
+    //check user reply is rock/paper/scissors, choose the winnder if it is, or inform the user if it's not
+    function checkValidUserChoice(userChoice, computerChoice) {
+    let invalidMessage = `invalid entry, please choose one of rock, paper, or scissors next time.`
+        if ( !( userChoice === "rock" || userChoice === "paper" || userChoice === "scissors" ) ) {
+            alert(invalidMessage);
+            console.warn(invalidMessage);
+        } else {
+            roundWinner(userChoice, computerChoice);
+        }
+    }
+
+    // determine who "wins" this round and increment appropriate tracking variable
+    // also doublechecks that user entered a valid choice
+    function roundWinner(userChoice, computerChoice) {
+        if (userChoice === computerChoice) {
+            console.log("You Tie.");
+            ties += 1;
+        // } else if ( !( userChoice === "rock" || userChoice === "paper" || userChoice === "scissors" ) ) {
+        //     console.warn(`invalid entry, please choose one of rock, paper, or scissors next time.`); //checks user responce is a valid choice
+        } else if ( userChoice === "rock" && computerChoice === "scissors" 
+            || userChoice === "paper" && computerChoice === "rock" 
+            || userChoice === "scissors" && computerChoice === "paper" ) {
+                userWins += 1;
+                console.log(`You choose ${userChoice}, computer choose ${computerChoice}, You win!`);       
+        } else {
+            computerWins += 1;
+            console.log(`You choose ${userChoice}, computer choose ${computerChoice}, You loose.`);
+        } 
     }
 
     playRound();
@@ -69,18 +85,7 @@ function playGame() {
 
     let scoreMessage = `user Wins:${userWins} computer Wins:${computerWins} ties:${ties}`;
     console.log(`final scores: ${scoreMessage}`);
+
 }
 
 playGame()
-
-/*
-ONCE have UI:
-reset game?
-----
-CREATE function reset
-    SET variables userWins computerWins and ties to 0
-    SET variable message to "lets play again"
-
-----------------------
-these are all the parts, but something is wonky w/ stringing them together
-    might just be b/c its not supposed to reset until 5 loops, but as I know single rounds arent the goal, its not really worth spending time puzzling it out. */ 
