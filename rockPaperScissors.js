f139a04da8eee80dc450c1f28bdec18da89a8487
SilverRowan <silverrowan@gmail.com>
2025-11-07 23:42:26 -0800

    Encolse computer choice in own function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,23 +1,32 @@
 // Computer Choice
 // ----
 
-//declare function: get integer between values a and b, inclusive, a = lowest value, b=highest value
-function randomInteger(a, b) { 
-    return Math.floor( Math.random() * b + a );
+//Create function getComputerChoice
+
+function getComputerChoice() {
+
+    //declare function: get integer between values a and b, inclusive, a = lowest value, b=highest value
+    function randomInteger(a, b) { 
+        return Math.floor( Math.random() * b + a );
+    }
+
+    //get randomInteger from 1-3, assign to computerChoice
+    let random123 = randomInteger( 1, 3 ); 
+    let randomRockPaperScissors;
+
+    //use computerChoice number to set Rock/paper/scissors
+    if (random123 === 1) { 
+        randomRockPaperScissors = "rock";
+    } else if (random123 === 2) {
+        randomRockPaperScissors = "paper";
+    } else if (random123 === 3) {
+        randomRockPaperScissors = "scissors";
+    } else {console.log("variable randomNumber out of bounds");}
+    console.log(randomRockPaperScissors);
+    return randomRockPaperScissors;
 }
+let computerChoice = getComputerChoice();
 
-//get randomInteger from 1-3, assign to computerChoice
-let computerChoice = randomInteger( 1, 3 ); 
-
-//use computerChoice number to set Rock/paper/scissors
-if (computerChoice === 1) { 
-    computerChoice = "rock";
-} else if (computerChoice === 2) {
-    computerChoice = "paper";
-} else if (computerChoice === 3) {
-    computerChoice = "scissors";
-} else {console.log("variable randomNumber out of bounds");}
-console.log(computerChoice);
 
 // Get Human Choice 
 // ----
@@ -82,4 +91,4 @@ CREATE function reset
 
 ----------------------
 these are all the parts, but something is wonky w/ stringing them together
-    might just be b/c its not supposed to reset until 5 loops, but as I know single rounds arent the goal, its not really worth spending time puzzling it out. */
+    might just be b/c its not supposed to reset until 5 loops, but as I know single rounds arent the goal, its not really worth spending time puzzling it out. */ 
